982d5e05101666927c8c2a45f17ffd248dafc051
Martin Stroet <martin.stroet@gmail.com>
2017-10-05 20:30:14 +1000

    Tidy up conservative adjustment

--- integration_error_estimate.py
+++ integration_error_estimate.py
@@ -88,13 +88,10 @@ def trapz_integrate_with_uncertainty(xs, ys, es, be_conservative=True):
     sum_gap_errors = np.abs(np.sum(gap_errors))
 
     max_interval_error = np.max(np.abs(np.concatenate((forward_results[-1], backwards_results[-1]))))
-    forward_reverse_diff = np.abs(np.abs(np.sum(forward_results[-1])) - np.abs(np.sum(backwards_results[-1])))
 
-    error_safety_policy = max(max_interval_error, forward_reverse_diff)
+    total_error = point_uncertainty_error + sum_gap_errors + (max_interval_error if be_conservative else 0.0)
 
-    total_error = point_uncertainty_error + sum_gap_errors + (error_safety_policy if be_conservative else 0.0)
-
-    return np.trapz(ys, xs), total_error, gap_xs, gap_ys, gap_errors, integration_point_errors, error_safety_policy
+    return np.trapz(ys, xs), total_error, gap_xs, gap_ys, gap_errors, integration_point_errors, max_interval_error
 
 def config_argparse():
     argparser = argparse.ArgumentParser(description='Integration Error Estimate')
